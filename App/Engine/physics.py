0c8e61e0102fff41e186689915b149d1828e0b2c
reader124 <sereading9999@gmail.com>
2024-03-04 11:11:59 -0700

    started building the physics class

--- App/Engine/physics.py
+++ App/Engine/physics.py
@@ -7,4 +7,14 @@
 #
 ################################################################################
 
+# imports
+import pygame
+from OpenGL.GL import *
+from OpenGL.GLU import gluPerspective
+
+class Physics():
+    def __init__(self):
+        #do stuff
+        x = 1 + 1
+
 if __name__ == '__main__': assert False, 'This is a class file. Import its contents into another file.'
